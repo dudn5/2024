bcb5b80031fb4533fb15ac91b50e7fa43e7483be
dudn5 <163218280+dudn5@users.noreply.github.com>
2024-05-08 12:25:31 +0900

    Add files via upload

---
+++ AbstractClassDemo.java
@@ -0,0 +1,18 @@
+package sec01;
+
+public class AbstractClassDemo {
+
+	public static void main(String[] args) {
+		// shape s =new Shape();
+		
+	
+		Circle c = new Circle(3);
+		c.draw();
+		System.out.printf("원의 넓이는 %.1f\n",c.findArea());
+		
+		
+		
+		
+	}
+
+}
